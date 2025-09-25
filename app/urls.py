9a0e1db8362bbe9ed002efb33ec7ae10ce0c1354
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-09-25 10:48:21 +0530

    CRUD Operation

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path('',views.main),
-    path('home/', views.index),
-    path('home/details/<int:id>',views.details),
-    path('testing/',views.testing)
+    path('home/', views.index,name='home'),
+    path('home/details/<int:id>',views.details, name='details'),
+    path('testing/',views.testing),
+    path('add/',views.add, name='add'),
+    path('add_record/',views.add_record, name='add_record'),
+    path('update/<int:id>/',views.update, name='update'),
+    path('delete/<int:id>/',views.delete, name='delete')
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from app.models import App
 
@@ -6,7 +6,7 @@ from app.models import App
 
 def index(request):
     person = App.objects.all().values()
-    return render(request,'home.html',{'person':person})
+    return render(request,'index.html',{'person':person})
     # return HttpResponse("Hello World")
 
 def details(request,id):
@@ -21,3 +21,29 @@ def testing(request):
     'fruits': ['Apple', 'Banana', 'Cherry'],   
   }
     return render(request,'testing.html',context)
+
+def add(request):
+    return render(request,'add.html')
+
+def add_record(request):
+    fname = request.POST['fname']
+    lname = request.POST['lname']
+    new_member = App(firstname=fname, lastname=lname)
+    new_member.save()
+    return redirect('home')
+
+def update(request,id):
+    person = App.objects.get(id=id)
+    if request.method == "POST":
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        person.firstname = fname
+        person.lastname = lname
+        person.save()
+        return redirect('home')
+    return render(request,'update.html', {'person' : person})
+
+def delete(request,id):
+    person = App.objects.get(id=id)
+    person.delete()
+    return redirect('home')
